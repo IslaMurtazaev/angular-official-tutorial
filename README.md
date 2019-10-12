137375d1705c79b1e627f6df20a5093bdd1f86d6
IslaMurtazaev <islam.muratazaev@gmail.com>
2019-10-12 14:05:47 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-akql1w
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-akql1w)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from "@angular/core";
+
+@Component({
+  selector: "app-product-alerts",
+  templateUrl: "./product-alerts.component.html",
+  styleUrls: ["./product-alerts.component.css"]
+})
+export class ProductAlertsComponent implements OnInit {
+  @Input() product;
+  constructor() {}
+
+  ngOnInit() {}
+}
